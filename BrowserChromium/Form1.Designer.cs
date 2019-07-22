847acec028dd2c7d5f0cf274608fac43d2293aa1
Rodrigo <rodrigo00737@gmail.com>
2019-07-21 22:14:29 -0300

    Primeiro commit , subindo app

---
+++ BrowserChromium/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BrowserChromium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btGo = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lbUrl = new System.Windows.Forms.Label();
+            this.btNewTab = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(50, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(472, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btGo
+            // 
+            this.btGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btGo.Location = new System.Drawing.Point(539, 9);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(75, 23);
+            this.btGo.TabIndex = 1;
+            this.btGo.Text = "GO";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // lbUrl
+            // 
+            this.lbUrl.AutoSize = true;
+            this.lbUrl.Location = new System.Drawing.Point(12, 15);
+            this.lbUrl.Name = "lbUrl";
+            this.lbUrl.Size = new System.Drawing.Size(32, 13);
+            this.lbUrl.TabIndex = 2;
+            this.lbUrl.Text = "URL:";
+            // 
+            // btNewTab
+            // 
+            this.btNewTab.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btNewTab.Location = new System.Drawing.Point(620, 9);
+            this.btNewTab.Name = "btNewTab";
+            this.btNewTab.Size = new System.Drawing.Size(75, 23);
+            this.btNewTab.TabIndex = 3;
+            this.btNewTab.Text = "New Tab";
+            this.btNewTab.UseVisualStyleBackColor = true;
+            this.btNewTab.Click += new System.EventHandler(this.btNewTab_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(365, 171);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 267);
+            this.panel1.TabIndex = 0;
+            this.panel1.Visible = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 38);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(677, 400);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(669, 374);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 443);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btNewTab);
+            this.Controls.Add(this.lbUrl);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chromium";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btGo;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lbUrl;
+        private System.Windows.Forms.Button btNewTab;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ BrowserChromium/Form1.cs
@@ -0,0 +1,80 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowserChromium
+{
+    public partial class Form1 : Form
+    {
+        //ChromiumWebBrowser chrome;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+
+            Cef.Initialize(settings);
+            textBox1.Text = "http://www.google.com";
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser(textBox1.Text);
+            chrome.Parent = tabControl1.SelectedTab;
+            //this.panel1.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.TitleChanged += Chrome_TitleChanged;
+        }
+
+        private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                textBox1.Text = e.Address;
+            }));
+        }
+
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)  
+                chrome.Load(textBox1.Text);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void btNewTab_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "New Tab";
+            tabControl1.Controls.Add(tab);
+            tabControl1.SelectTab(tabControl1.TabCount - 1);
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser("http://www.google.com");
+            chrome.Parent = tab;
+            chrome.Dock = DockStyle.Fill;
+            textBox1.Text = "http://www.google.com";
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.TitleChanged += Chrome_TitleChanged;
+        }
+
+        private void Chrome_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                tabControl1.SelectedTab.Text = e.Title;
+            }));
+        }
+    }
+}
